541d0851a0fbfd8610d9ea87bc0c3774d3396a4d
Benjamin Buckmaster <benjaminbuckmaster@icloud.com>
2024-02-05 19:14:24 +1100

    added and configured item category table

--- hommus/item/models.py
+++ hommus/item/models.py
@@ -2,3 +2,10 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=255)
+
+    class Meta:
+        verbose_name_plural = 'Categories' # make django show categories instead of categorys
+        ordering = ('name',) # show in alphabetical descending order
+
+    def __str__(self) -> str:
+        return self.name
