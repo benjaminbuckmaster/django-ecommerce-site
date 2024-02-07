f6478285d68c7bfcab6ea34d739f4a82176b2765
Benjamin Buckmaster <benjaminbuckmaster@icloud.com>
2024-02-07 15:18:52 +1100

    created signup page

--- hommus/core/views.py
+++ hommus/core/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 
 from item.models import Category, Item
 
+from .forms import SignUpForm
+
 def index(request):
     items = Item.objects.filter(is_sold=False)[0:6]
     categories = Category.objects.all()
@@ -13,3 +15,10 @@ def index(request):
 
 def contact(request):
     return render(request, 'core/contact.html')
+
+def signup(request):
+    form = SignUpForm()
+
+    return render(request, 'core/signup.html', {
+        'form': form
+    })
