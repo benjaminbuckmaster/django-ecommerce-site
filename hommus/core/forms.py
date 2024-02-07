306263d900299be3ae3cd846ec3f9654735d971a
Benjamin Buckmaster <benjaminbuckmaster@icloud.com>
2024-02-07 15:38:00 +1100

    created signup function

--- hommus/core/forms.py
+++ hommus/core/forms.py
@@ -11,3 +11,23 @@ class SignUpForm(UserCreationForm):
             'password1',
             'password2'
         )
+
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder': 'username',
+        'class': 'w-full py-4 px-6 rounded-xl'
+    }))
+
+    email = forms.CharField(widget=forms.EmailInput(attrs={
+        'placeholder': 'email',
+        'class': 'w-full py-4 px-6 rounded-xl'
+    }))
+
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'password',
+        'class': 'w-full py-4 px-6 rounded-xl'
+    }))
+
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'repeat password',
+        'class': 'w-full py-4 px-6 rounded-xl'
+    }))
